03f74226b28f43cc786a5ad4bc2041377b165ccc
gayatreesorte24 <gayatreesorte@gmail.com>
2023-02-08 19:10:29 +0530

    Initial Project Structure for BackEnd

---
+++ blogging-app-backend/src/test/java/com/example/bloggingappbackend/BloggingAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bloggingappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggingAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
